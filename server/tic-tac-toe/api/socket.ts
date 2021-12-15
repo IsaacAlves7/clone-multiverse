b380f6a35248f19fb1198f371399ca409fc68468
Isaac Alves <isaac.alves0720@gmail.com>
2021-12-14 22:35:46 -0300

    cors

--- server/tic-tac-toe/api/socket.ts
+++ server/tic-tac-toe/api/socket.ts
@@ -2,6 +2,11 @@ import { Server } from "socket.io";
 
 export default (httpServer) => {
     const io = new Server(httpServer, { 
-        
+        cors: {
+            origin: "*"
+        },
     });
+
+    return io;
+    
 } 

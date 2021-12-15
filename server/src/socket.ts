66f15f83ce588cdc245ded1b1263c4cd2498849a
Isaac Alves <isaac.alves0720@gmail.com>
2021-12-15 16:36:26 -0300

    Everything updated to localhost

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -2,15 +2,15 @@ import { useSocketServer } from "socket-controllers";
 import { Server } from "socket.io";
 
 export default (httpServer) => {
-    const io = new Server(httpServer { 
+    const io = new Server(httpServer, { 
         cors: {
             origin: "*",
         },
     });
 
-    io.on("connection", (socket) => {
+    // io.on("connection", (socket) => {
         
-    });
+    // });
 
     useSocketServer(io, { controllers: [__dirname + "/api/controllers/*.ts"] }); // use all TS socket-controllers in this directory!
 
